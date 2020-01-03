388abb90621a046af901c8fcf88e18d9b3f544e9
Yipeng Hu <yipeng.hu@ucl.ac.uk>
2020-01-03 20:52:57 +0000

    ref #1 adding checking plot for multiple subject

--- script_ckeckData.py
+++ script_ckeckData.py
@@ -4,16 +4,23 @@ import tensorflow as tf
 from matplotlib import pyplot as plt
 
 
-norm_folder = 'Scratch/data/protocol/normalised'
+norm_folder = '/mnt/c/Users/yhu/Scratch/data/protocol/normalised'
 if os.name == 'nt':
     home_dir = os.path.expanduser('~')
 else:
     home_dir = os.environ['HOME']
 
 filename = os.path.join(home_dir, norm_folder, 'protocol_sweep_class_subjects.h5')
-group_name = '/subject%06d_frame%08d' % (20, 10)
-frame = tf.keras.utils.HDF5Matrix(filename, group_name)
 
+idx_subject = [20, 50, 150, 200]
+idx_frame = [0, 15, 20]
+nSbj = len(idx_subject)
+nFrm = len(idx_frame)
 plt.figure()
-plt.imshow(frame, cmap='gray')
+for iSbj in range(nSbj):
+    for iFrm in range(nFrm):
+        group_name = '/subject%06d_frame%08d' % (idx_subject[iSbj], idx_frame[iFrm])
+        frame = tf.keras.utils.HDF5Matrix(filename, group_name)
+        plt.subplot(nSbj, nFrm, iSbj*nFrm+iFrm+1)
+        plt.imshow(frame, cmap='gray')
 plt.show()
